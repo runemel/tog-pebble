8d20a302cff839b20bee22544d525bbcf522550a
runemel <runemel@gmail.com>
2015-11-02 16:48:04 +0100

    Add favorite menu
    No functionality yet

---
+++ src/favoritesMenu.js
@@ -0,0 +1,37 @@
+var UI = require('ui');
+var Settings = require('settings');
+
+var favorites = [];
+
+var favoritesMenu = new UI.Menu({
+  sections: [{
+    title: 'Favoritter',
+    items: favorites
+  }]
+});
+favoritesMenu.on('select', function(e) {
+  var selectedStation = favorites[e.itemIndex];
+  var stationMenu = require('stationMenu');
+  stationMenu.show(selectedStation);
+});
+
+var fillStations = function(savedFavorites){
+  favorites.length = 0;
+  for(var i in savedFavorites){
+    var station = savedFavorites[i];
+    favorites.push({title: station.Name, stopPointRef: station.StopPointRef});
+  }
+};
+
+exports.show = function(){
+  var savedFavorites = Settings.data('favorites');
+  if(!savedFavorites){
+    savedFavorites = [];
+    Settings.data('favorites', savedFavorites);
+  }
+  else if(savedFavorites.length > 0){
+    fillStations(savedFavorites);
+  }
+  
+  favoritesMenu.show();
+};
--- src/mainMenu.js
+++ src/mainMenu.js
@@ -3,6 +3,9 @@ var UI = require('ui');
 var menuItems = [
   {
     title: 'Stasjoner'
+  },
+  {
+    title: 'Favoritter'
   }
   
 ];
@@ -11,13 +14,15 @@ var menu = new UI.Menu({
   sections: [{
     items: menuItems
   }]
-  });
+});
 menu.on('select', function(e) {
   if(e.itemIndex === 0){
     var alphabetMenu = require('alphabetMenu');
     alphabetMenu.show();
   }
   else if(e.itemIndex === 1){
+    var favoritesMenu = require('favoritesMenu');
+    favoritesMenu.show();
   }
 });
 
